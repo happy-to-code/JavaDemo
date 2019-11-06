91e5b2331407ae92b43044283b003122cadd85f7
yida <zhangyfei126@yeah.net>
2019-11-06 20:10:44 +0800

    init

--- src/cn/test3/niuke/Solution1.java
+++ src/cn/test3/niuke/Solution1.java
@@ -11,13 +11,12 @@ import java.util.List;
  * 并期望旁边的正数会弥补它呢？例如:{6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0个开始,到第3个为止)。
  * 给一个数组，返回它的最大连续子序列的和，你会不会被他忽悠住？(子向量的长度至少是1)
  */
+
 public class Solution1 {
     public int FindGreatestSumOfSubArray(int[] array) {
         int max = array[0];
         int sum = 0;
-
         int[] clone = array.clone();
-
         Arrays.sort(clone);
 
         if (clone[0] >= 0) {
