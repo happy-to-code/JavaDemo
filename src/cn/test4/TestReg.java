16a91fbaa72c3c234ccb8326b7460b0086775f56
yida <zhangyfei126@yeah.net>
2020-03-24 09:53:49 +0800

    更新

--- src/cn/test4/TestReg.java
+++ src/cn/test4/TestReg.java
@@ -1,7 +1,5 @@
 package cn.test4;
 
-import java.util.Date;
-
 /**
  * @Describle:
  * @Author: zhangyifei
@@ -9,16 +7,12 @@ import java.util.Date;
  */
 public class TestReg {
     public static void main(String[] args) {
-
-        System.out.println((new Date()).getTime());
         String s = " 1 04228陈2   先生1";
 
         String newStr = s.trim().replaceAll(" ", "");
         System.out.println("s1 = " + newStr);
 
-
         StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < newStr.length(); i++) {
             if (newStr.charAt(i) < 48 || newStr.charAt(i) > 57) {
                 break;
